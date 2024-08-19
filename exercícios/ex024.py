298cfa30f2707809980ee1eacad2a347a64f6e74
Jackson Fidelix <jcksfidelix@gmail.com>
2024-08-19 13:50:49 -0300

    correções de exercicios

--- exercícios/ex024.py
+++ exercícios/ex024.py
@@ -1,4 +1,4 @@
 name = input('Digite nome completo de sua cidade: ')
 editedname = name.split()
 print('O nome inteiro de sua cidade é {}'.format(name))
-print('Seu primeiro nome é: {}'.format(editedname[0]) if editedname[0] == 'São' else 'Seu primerio nome não começa com a palavra São!')
+print('Seu primeiro nome é: {}'.format(editedname[0]) if editedname[0].upper() == 'SÃO' else 'Seu primerio nome não começa com a palavra São!')
--- exercícios/ex025.py
+++ exercícios/ex025.py
@@ -1,3 +1,3 @@
 name = input('Digite seu nome completo: ')
 splitname = name.split()
-print('Seu nome é: {}'.format(name), 'e por óbvio contém a palavra Silva!' if 'Silva' in name else '! \n Você não possui o nome Silva!')
+print('Seu nome é: {}'.format(name), 'e por óbvio contém a palavra Silva!' if 'SILVA' in name.upper() else '! \n Você não possui o nome Silva!')
