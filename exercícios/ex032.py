3c948b92ed06e4753d599e86cb2b92750138c493
Jackson Fidelix <jcksfidelix@gmail.com>
2024-08-29 14:47:44 -0300

    Update ex032.py

--- exercícios/ex032.py
+++ exercícios/ex032.py
@@ -1,5 +1,8 @@
-ano = int(input('Digite um ano qualquer: '))
-if ano % 4 == 0:
-    print('O ano digitado foi {} e ele é um ano bissexto!'.format(ano))
+from datetime import date
+ano = int(input('Digite um ano qualquer ou digite "0" para selecionar o ano atual: '))
+if ano == 0:
+    ano = date.today().year
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print('O ano digitado foi {} E ELE É um ano bissexto!'.format(ano))
 else:
-    print('O ano {} NÃO é um ano biossexto!'.format(ano))
+    print('O ano {} NÃO é um ano bissexto!'.format(ano))
