b64412aa394e94a78cd6d129c80f5d9b5550cd86
Jackson Fidelix <jcksfidelix@gmail.com>
2024-08-29 23:06:30 -0300

    Image of colors

---
+++ pythonTeste/aula011.py
@@ -0,0 +1,11 @@
+#coloque como string a formatação
+
+print('\033[1;33;47mOlá, Mundo!\033[m')
+
+a = 3
+b =5
+print('Os valores são \033[33m{}\033[m e \033[31m{}\033[m!'.format(a, b))
+
+nome = 'Fidelix'
+print('Olá! Muito prazer em te conhecer {}{}{}!!!'.format('\033[4;36m',nome, '\033[m'))
+
