f51403a113c6cd19da5860709f74e0e728dd48a8
Jackson Fidelix <jcksfidelix@gmail.com>
2024-09-17 22:00:00 -0300

    exercice forty-four

---
+++ exercícios/ex044.py
@@ -0,0 +1,8 @@
+preco_produto = float(input('Digite o preço do produto: '))
+print('SEGUE AS MODALIDADES DE PAGAMENTO:')
+print('01 - PARA MODALIDADE À VISTA')
+print('02 - PARA MODALIDADE À VISTA NO CARTÃO')
+print('03 - PARA MODALIDADE ATÉ 2X NO CARTÃO')
+print('04 - PARA MODALIDADE 3X OU MAIS NO CARTÃO')
+print('===--===--===--===--===--===--===--===--===')
+print
