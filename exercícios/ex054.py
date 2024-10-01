6ba16de069c270ac1c28cf37949e4cb3d45f84b1
Jackson Fidelix <jcksfidelix@gmail.com>
2024-10-01 20:15:29 -0300

    Update exercicie fifty-four

--- exercícios/ex054.py
+++ exercícios/ex054.py
@@ -3,10 +3,10 @@ from datetime import date
 maioridade = 0
 minoridade = 0
 for i in range(1, 7+1):
-    ano = int(input('Digite seu ano de nascimento:'))
+    ano = int(input(f'Digite ano de nascimento da {i}ª pessoa: '))
     idade = date.today().year - ano
     if idade >= 21:
         maioridade += 1
     else:
         minoridade += 1
-print(f'A quantidade de pessoas que atingiram sua MAIORIDADE é \033[1;32m{maioridade}\033[m e a MINORIDADE é \033[1;31m{minoridade}\033[m!')
+print(f'A quantidade de pessoas que atingiram sua MAIORIDADE é \033[1;32m{maioridade}\033[m e há \033[1;31m{minoridade}\033[m pessoa(s) ainda com MINORIDADE!')
