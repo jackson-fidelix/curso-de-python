12ae93319b302c47f7d51500c075959a6530241b
Jackson Fidelix <116315846+jackson-fidelix@users.noreply.github.com>
2024-11-26 15:51:05 -0300

    Update ex078.py

--- exercícios/ex078.py
+++ exercícios/ex078.py
@@ -3,8 +3,22 @@
 
 valores = []
 for i in range(0, 5):
-    valores.append(int(input('Digite um valor:')))
+    valores.append(int(input('Digite um valor: ')))
+
+maior_valor = max(valores)
+menor_valor = min(valores)
+
+posicoes_maior = []
+for i in range(len(valores)):
+    if valores[i] == maior_valor:
+        posicoes_maior.append(i)
+
+posicoes_menor = []
+for i in range(len(valores)):
+    if valores[i] == menor_valor:
+        posicoes_menor.append(i)
+
 print('===-'*12)
 print(f'Essa é a lista completa: {valores}')
-print(f'O maior valor é {max(valores)} e está na {valores.index(max(valores))+1}° posição.')
-print(f'O menor valor é {min(valores)} e está na {valores.index(min(valores))+1}° posição.')
+print(f'O maior valor é {max(valores)} e está nas posições {' ... '.join(map(str, posicoes_maior))}')
+print(f'O menor valor é {min(valores)} e está nas posições {' ... '.join(map(str, posicoes_menor))}')
