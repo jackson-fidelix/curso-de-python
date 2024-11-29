c611c72d605bf1530a32c76b61e8ba126534a734
Jackson Fidelix <116315846+jackson-fidelix@users.noreply.github.com>
2024-11-29 15:51:42 -0300

    Update ex078.py

--- exercícios/ex078.py
+++ exercícios/ex078.py
@@ -3,7 +3,7 @@
 
 valores = []
 for i in range(0, 5):
-    valores.append(int(input('Digite um valor: ')))
+    valores.append(int(input(f'Digite um valor na posição {i}: ')))
 
 maior_valor = max(valores)
 menor_valor = min(valores)
@@ -22,3 +22,9 @@ print('===-'*12)
 print(f'Essa é a lista completa: {valores}')
 print(f'O maior valor é {max(valores)} e está nas posições {' ... '.join(map(str, posicoes_maior))}')
 print(f'O menor valor é {min(valores)} e está nas posições {' ... '.join(map(str, posicoes_menor))}')
+
+# Nesta solução, poderíamos usar também o enumerate, para trazer as posições.
+# faríamos uma comparação com for e traríamos as posição que esse valor aparece
+# exemplo: for i, v in enumerate(valores):
+# if v == maior_valor:
+# print(f'{i}...', end=" ") 
