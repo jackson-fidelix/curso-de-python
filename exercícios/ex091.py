cea0ac5a8783d095949026bdfe4598fcbe9d2b5e
Jackson Fidelix <116315846+jackson-fidelix@users.noreply.github.com>
2024-12-30 15:19:42 -0300

    Update ex091.py

--- exercícios/ex091.py
+++ exercícios/ex091.py
@@ -3,19 +3,28 @@
 # dicionário.No final, coloque esse dicionário em ordem, 
 # sabendo que o vencedor tirou o maior número no dado.
 from random import randint
+from time import sleep
 
 totjogadores = []
 for i in range(1,5):
     jogador = {}
     num_dado = randint(1, 6)
-    print(f'O jogador {i} tirou {num_dado}')
+    sleep(1)
+    print(f'O jogador {i} está se preparando...')
+    sleep(2)    
+    print(f'O jogador {i} tirou {num_dado} no dado!')
     jogador['num_jogador'] = i
     jogador['num_dado'] = num_dado
     totjogadores.append(jogador.copy())
-print('-=-'*8)
+print('-=-'*10)
+
+#eu poderia importar ITEMGETTER from OPERATOR, caso estivesse em dict e não em list. Ex:
+#ranking = sorted(jogador.items(), key=itemgetter(1), reverse=True)
+#para funcionar todo os conteudo da lista deveria ser especificado, 
+# sem FOR, para não pegar apenas o último item do dicionário.
 
 lista_ordenada = sorted(totjogadores,reverse=True, key=lambda item:item['num_dado'])
 
 for index, value in enumerate(lista_ordenada):
     print(f'{index + 1}ºLugar - player {value['num_jogador']} com {value['num_dado']}')
-    print('---'*8)
+    print('---'*9)
