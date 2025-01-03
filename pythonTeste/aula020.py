85f11f184527601eb45380a977c0eac0c4780b3c
Jackson Fidelix <116315846+jackson-fidelix@users.noreply.github.com>
2025-01-03 16:32:57 -0300

    Update aula020.py

--- pythonTeste/aula020.py
+++ pythonTeste/aula020.py
@@ -1,13 +1,41 @@
-def soma(a, b):
-    soma = a + b
-    print(soma)
+def dobra(list):
+    pos = 0
+    while pos < len(list):
+        list[pos] *= 2
+        pos += 1
+
+
+valores = [6, 3, 9, 1, 0, 2]
+dobra(valores)
+print(valores)
+
+
+
+#Quarta explicação
+#def contador(* núm):
+    #for valor in núm:
+    #    print(f' {valor} ', end="")
+    #print('\n == FIM == ')
+#   tam = len(núm)
+#    print(f'Recebi os valores {núm} e são ao todo {tam} números!')
+
+
+#contador(2, 1, 7)
+#contador(8, 0)
+#contador(4, 4, 7, 6, 2)
+
+
+#Terceira explicação
+#def soma(a, b):
+#    soma = a + b
+#    print(soma)
 
 #se não explicitar, o primeiro valor vai para A e o segundo para B
-soma(4, 5)
-soma(8, 9)
+#soma(4, 5)
+#soma(8, 9)
 # ou explicitar
-soma(a = 2, b = 1)
-soma(b = 4, a = 1)
+#soma(a = 2, b = 1)
+#soma(b = 4, a = 1)
 
 
 #Segunda Explicação
