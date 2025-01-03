7dba7beed0dcff4f6ac828c8933139ecd3a191c0
Jackson Fidelix <116315846+jackson-fidelix@users.noreply.github.com>
2025-01-03 12:49:25 -0300

    Create aula020.py

---
+++ pythonTeste/aula020.py
@@ -0,0 +1,28 @@
+def título(txt):
+    print('-'*30)
+    print(txt)
+    print('-'*30)
+
+#Segunda Explicação
+título('        CURSO EM VÍDEO  ')
+título('        APRENDA PYTHON  ')
+título('        JACKSON FIDELIX  ')
+
+
+
+#primeira explicação
+#def lin():
+#    print('-'*30)
+
+
+#Programa Principal
+#lin()
+#print('        CURSO EM VÍDEO  ')
+#lin()
+#lin()  
+#print('        APRENDA PYTHON  ')
+#lin()
+#lin()
+#print('        APRENDA PYTHON  ')  
+#lin()
+#---------------------
