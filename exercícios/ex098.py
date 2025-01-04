016867e9694be533c0493c64932e3c275e1bff45
Jackson Fidelix <116315846+jackson-fidelix@users.noreply.github.com>
2025-01-04 11:34:17 -0300

    Create ex098.py

---
+++ exercícios/ex098.py
@@ -0,0 +1,32 @@
+#Faça um programa que tenha uma função chamada contador(), que receba três
+#parâmetros: início, fim e passo e realize a contagem. Seu programa tem que 
+#realizar três contagens através da função criada:
+#A)De 1 até 10, de 1 em 1
+#B)De 10 até 0, de 2 em 2
+#C) uma contagem personalizada.
+
+def contador(início,fim, passo):
+    if passo == 0:
+        passo = 1 
+    if início > fim or passo < 0: 
+        passo_ajustado =  (passo + passo) - passo 
+        print('-=-='*8)
+        print(f'Contagem de {início} até {fim} de {passo_ajustado} em {passo_ajustado}')
+        for c in range(início, fim - 1, -passo):
+            print(f'{c}', end=" ")
+        print('FIM!')
+        print('-=-='*8)
+    else:  
+        print(f'Contagem de {início} até {fim} de {passo} em {passo}')
+        for c in range(início, fim + 1, passo):
+            print(f'{c}', end=" ")
+        print('FIM!')
+        print('-=-='*8)
+
+
+contador(1, 10, 1)
+contador(10, 0, -2)
+inicial   = int(input('INÍCIO: '))
+final = int(input('FIM: '))
+passo = int(input('PASSO: '))
+contador(inicial, final, passo)
