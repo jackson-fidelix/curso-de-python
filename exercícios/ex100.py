b303c7cdf0420c5535e62c2631413c31c8f3c770
Jackson Fidelix <jcksfidelix@gmail.com>
2025-01-06 01:20:49 -0300

    Create ex100.py

---
+++ exercícios/ex100.py
@@ -0,0 +1,28 @@
+#Faça um programa que tenha uma lista chamada números e duas funções chamadas
+#sorteio() e somaPar(). A primeira função vai sortear 5 números e vai 
+#colocá-los dentro da lista e a segunda função vai mostrar a soma 
+#entre todos os valores PARES sorteados pela função anterior.
+from random import randint
+
+
+def sorteio(num):
+    print('-=-='*15)
+    print('Sorteando 5 valores da lista: ', end=" ")
+    for i in range(num):
+        sort = randint(0, 10)
+        print(f'{sort}', end=" ")
+        numeros.append(sort)
+    print('PRONTO!')
+
+def somaPar(txt):
+    soma_pares = 0
+    for i, v in enumerate(numeros):
+        if v % 2 == 0:
+            soma_pares += v
+    print(txt, end="")
+    print(soma_pares)
+
+    
+numeros = []
+sorteio(5)
+somaPar(f'Somando os valores pares de {numeros}, temos ')
