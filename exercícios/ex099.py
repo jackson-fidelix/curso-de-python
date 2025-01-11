18d26009410023e083fc303ebb0a78667b532b08
Jackson Fidelix <jcksfidelix@gmail.com>
2025-01-10 23:59:52 -0300

    Update ex099.py

--- exercícios/ex099.py
+++ exercícios/ex099.py
@@ -8,17 +8,17 @@ from time import sleep
 def maior(rand):
     num_maior = 0
     print('-=-='*15)
-    print('Atuallizando os valores...', flush=True)
+    print(f'Atualizando os valores passados ...', flush=True)
     for i in range(rand):
         values = randint(1, 10)
         sleep(0.5)
-        print(f'{values}',flush=True, end=" ")
+        print(f'\033[1;43m {values} \033[m',flush=True, end=" ")
         if num_maior == 0:
             num_maior = values
         elif values > num_maior:
             num_maior = values
-    print(f'Foram informados {rand} valores ao todo.')
-    print(f'O maior valor informado foi {num_maior}.')
+    print(f'Foram informados \033[1;41m {rand} \033[m valores ao todo.')
+    print(f'O maior valor informado foi \033[1;42m {num_maior} \033[m.')
 
 
 maior(randint(0, 10))
