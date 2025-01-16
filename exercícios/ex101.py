d9894347cc08806286f08f28ec1e670cc0ae4c18
Jackson Fidelix <jcksfidelix@gmail.com>
2025-01-16 09:37:06 -0300

    Create ex101.py

---
+++ exercícios/ex101.py
@@ -0,0 +1,17 @@
+#crie um programa que tenha i,a função chamada "voto()" que vai receber como
+#parâmetro o ano de nascimento de uma pessoa, retornando um valor literal 
+#indicando se uma pessoa tem voto NEGADO, OPCIONAL ou OBRIGATÓRIO nas eleições
+from datetime import date
+
+def voto(year):
+    age = date.today().year - year 
+    if 16 > age > 18 or age > 65:
+        print(f'Com {age} anos: VOTO OPCIONAL.')
+    elif 18 <= age <= 65:
+        print(f'Com {age} anos: VOTO OBRIGATÓRIO.')
+    else:
+        print(f'Com {age} anos: VOTO NEGADO.')
+        
+    
+nasc = int(input('Em que ano você nasceu: '))
+voto(nasc)
