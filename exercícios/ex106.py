2bdc83a91c0dfeb6ab4d7bf71dcad6c10592a9dd
Jackson Fidelix <116315846+jackson-fidelix@users.noreply.github.com>
2025-01-22 17:02:16 -0300

    Update ex106.py

--- exercícios/ex106.py
+++ exercícios/ex106.py
@@ -1,41 +1,31 @@
 #Faça um mini-programa que utilize o Interactive Help do Python. O usuário
 #vai digitar o comando e o manual vai aparecer. Quando o usuário digitar 
 #a palavra 'FIM', o programa se encerrará. OBS: Use cores.
-
-def ihpython(user = ""):
-    user == 'Início'
-    while user != "FIM":
-        msg_welcome = 'SISTEMA DE AJUDA PyHELP'
-        tam_welcome = len(msg_welcome) + 4
-        print('\033[7;32m')
-        print('~' * tam_welcome)
-        print('  SISTEMA DE AJUDA PyHELP')
-        print('~' * tam_welcome)
-        print('\033[m')
-
-        user = str(input('Função ou Biblioteca > '))
-        if user in "FimFinalizarfimFIM":
-            end = 'ATÉ LOGO'
-            tam_end = len(end) + 4
-            print('\033[7;31m')
-            print('~' * tam_end)
-            print('  ATÉ LOGO')
-            print('~' * tam_end)
-            print('\033[m')
-            break
-        else:
-            msg_access = f"Acessando o manual do '{user}'..."
-            tam_acces = len(msg_access) + 4
-            print('\033[7;34m')
-        print('~' * tam_acces)
-        print(f"Acessando o manual do '{user}'...")
-        print('~' * tam_acces)
-        print('\033[m')
-
-        print('\033[7m')
-        print(help(user))
-        print('\033[m')
+c = {
+    0: '\033[m',           # 0 - sem cores
+    1: '\033[0;30;41m'     # 1 - vermelho
+}
 
 
-user = "iniciar"
-ihpython(user)
+def ajuda(comando):
+    help(comando)
+    
+
+def titulo(msg, cor = 0):
+    tam = len(msg) + 4
+    print(c[cor], end="")
+    print('~' * tam)
+    print(msg)
+    print('~' * tam)
+    print(c[0], end="")
+
+
+user = ""
+while True:
+    titulo('  SISTEMA DE AJUDA PyHELP', 1)
+    user = str(input('Função ou Biblioteca > '))
+    if user.upper() == 'FIM':
+        break
+    else:
+        ajuda(user)
+titulo('  ATÉ LOGO', 1)
