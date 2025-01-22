fffd9b1ccaac288647800bed4b8f19fa3d845c0c
Jackson Fidelix <jcksfidelix@gmail.com>
2025-01-22 07:00:35 -0300

    Update ex103.py

--- exercícios/ex103.py
+++ exercícios/ex103.py
@@ -3,15 +3,18 @@
 #O programa deverá ser capaz de mostrar a ficha do jogador, mesmo 
 #que algum dado não tenha sido informado corretamente
 
-def ficha(name = "", goal = 0):
-    if not name:
-               name = "<desonhecido>"
-    if not goal:
-            goal = 0
+def ficha(name = "<desonhecido>", goal = 0):
     print(f'O jogador {name.upper()} fez {goal} gol(s) no campeonato.')
 
 
  #programa principal
-nome = input('Nome do Jogador: ').strip()
-gol = input('Número de Gols: ').strip()
-ficha(nome, gol)
+nome = str(input('Nome do Jogador: ')).strip()
+gol = str(input('Número de Gols: ')).strip()
+if gol.isnumeric():
+        gol = int(gol)
+else:
+        gol = 0
+if nome == '':
+        ficha(goal=gol)
+else:
+        ficha(nome,gol)
