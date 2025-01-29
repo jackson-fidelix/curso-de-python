f475be51017d1898562b54a270c8ff233bc0caae
Jackson Fidelix <116315846+jackson-fidelix@users.noreply.github.com>
2025-01-29 09:57:06 -0300

    Update principal.py

--- exercícios/ex108/principal.py
+++ exercícios/ex108/principal.py
@@ -1,10 +1,11 @@
-#Adicione ao módulo moedas.py criado nos desafios anteriores uma função chamada
-#resumo(), que mostre na tela algumas funções geradas pelas funç~pes que 
-#já temos no módulo criado até aqui
+#Adapte o código do desafio 107, criando uma função adicional chamada  
+#moeda() que consiga mostrar os valores coomo um valor monetário formatado 
 import sys
 sys.path.append('e:\MeusProjetos\curso-de-python\exercícios')
-from ex110 import moedas
-
+from ex108 import moedas
 
 price = float(input('Digite o preço: R$'))
-moedas.resumo(price, 80, 35)
+print(f'A metade de {moedas.moeda(price)} é {moedas.moeda(moedas.metade(price))}')
+print(f'O dobro de {moedas.moeda(price)} é {moedas.moeda(moedas.dobro(price))}')
+print(f'Aumentando 10%, temos {moedas.moeda(moedas.aumentar(price, 10))}')
+print(f'Reduzindo 13%, temos {moedas.moeda(moedas.diminuir(price, 13))}')
