9427b6985ad43e0cc935d16f8bdf3585db5e177a
Jackson Fidelix <116315846+jackson-fidelix@users.noreply.github.com>
2025-01-29 13:02:05 -0300

    Add files via upload

---
+++ exercícios/ex112/utilidadesCeV/principal.py
@@ -0,0 +1,14 @@
+# Dentro do pacote utilidadesCeV que criamos no desafio 112, temos um
+# módulo chamado dado.Crie uma função chamada leiaDinheiro() que seja capaz de
+# funcionar como a função input(), mas com uma validação de dados para aceitar
+# apenas valores que sejam monetários
+
+import sys
+#sys.path.append('e:\MeusProjetos\curso-de-python\exercícios')
+#from ex112.utilidadesCeV import moeda
+sys.path.append('c:\Windows\Temp')
+from ex112.utilidadesCeV import dado
+
+
+p = dado.leiaDinheiro('Digite o preço: R$')
+moeda.resumo(price, 80, 35)
