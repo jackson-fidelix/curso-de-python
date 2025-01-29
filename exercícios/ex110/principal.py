4872bbf9a1c7c7c5b353ce249e2af44ba0e43c3c
Jackson Fidelix <116315846+jackson-fidelix@users.noreply.github.com>
2025-01-29 09:57:32 -0300

    Update principal.py

--- exercícios/ex110/principal.py
+++ exercícios/ex110/principal.py
@@ -5,10 +5,6 @@ import sys
 sys.path.append('e:\MeusProjetos\curso-de-python\exercícios')
 from ex110 import moedas
 
+
 price = float(input('Digite o preço: R$'))
 moedas.resumo(price, 80, 35)
-# print(f'A metade de {moedas.moeda(price)} é {moedas.metade(price, False)}')
-# print(f'O dobro de {moedas.moeda(price)} é {moedas.dobro(price, True)}')
-# print(f'Aumentando 10%, temos {moedas.aumentar(price, 10, True)}')
-# print(f'Reduzindo 13%, temos {moedas.diminuir(price, 13, True)}')
-
