1ee757e1236fbf769bf33b8e21d36217f350dd84
Jackson Fidelix <116315846+jackson-fidelix@users.noreply.github.com>
2025-01-29 17:02:26 -0300

    Update __intit__ .py

--- exercícios/ex112/utilidadesCeV/dado/__intit__ .py
+++ exercícios/ex112/utilidadesCeV/dado/__intit__ .py
@@ -1,2 +1,9 @@
-def leiaDinheiro():
-    
+def leiaDinheiro(msg):
+    válido = False
+    while not válido:
+        entrada = str(input(msg)).replace(',','.').strip()
+        if entrada.isalpha() or entrada == "":     
+            print(f'\033[mErro: "{entrada}" não é um preço válido!\033[m')
+        else:
+            válido = True
+            return float(entrada)
