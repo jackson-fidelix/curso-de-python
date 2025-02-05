a377bbd65abf6d396b0d73ca61c078b82332575f
Jackson Fidelix <116315846+jackson-fidelix@users.noreply.github.com>
2025-02-05 10:28:04 -0300

    Update ex113.py

--- exercícios/ex113.py
+++ exercícios/ex113.py
@@ -3,18 +3,32 @@
 # Aproveite e crie também uma função leiaFloat() com a mesma funcionalidade.
 
 def leiaInt(msg):
-    while True:
+    while msg != int:
         try:
             n = int(input(msg))
             return n
-        except ValueError:
-            print(f'\033[1;31mERROR: por favor, digite um número inteiro válido.\033[m')
+        except (ValueError,TypeError):
+            print(f'\033[1;31mERRO: por favor, digite um número inteiro válido.\033[m')
         except KeyboardInterrupt:
             print(f'\033[1;31mERROR: O Usuário preferiu não digitar esse número.\033[m')
             n = 0
             break
-        else:
+    return n
 
+def leiaFloat(msg):
+    while msg != float:
+        try:
+            n = float(input(msg))
+            return n
+        except (ValueError, TypeError):
+            print('\033[1;31mERRO: por favor, digite um número real válido\033[m')
+        except KeyboardInterrupt:
+            print(f'\033[1;31mERROR: O Usuário preferiu não digitar esse número.\033[m')
+            n = 0
+            break
+    return n
 
-n = leiaInt('Digite um número: ')
-print(f'Você acabou de digitar o número {n}')
+        
+n1 = leiaInt('Digite um Inteiro: ')
+n2 = leiaFloat('Digite um Real: ')
+print(f'O valor inteiro digitado foi {n1} e o real {n2}')
