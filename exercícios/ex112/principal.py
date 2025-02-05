2ae5d21cdf67e86bfe04348ccf7e8d5a8965f3ad
Jackson Fidelix <jcksfidelix@gmail.com>
2025-02-04 21:49:58 -0300

    Update ex112

--- exercícios/ex112/principal.py
+++ exercícios/ex112/principal.py
@@ -1,7 +1,7 @@
 # Dentro do pacote utilidadesCeV que criamos no desafio 112, temos um
 # módulo chamado dado.Crie uma função chamada leiaDinheiro() que seja capaz de
 # funcionar como a função input(), mas com uma validação de dados para aceitar
-# apenas valores que sejam monetários
+# apenas valores que sejam monetários.
 import sys
 sys.path.append('e:\MeusProjetos\curso-de-python\exercícios')
 from ex112.utilidadesCeV import moeda
