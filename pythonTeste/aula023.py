670cada0c83817f0a4b446377682f2321f891d8c
Jackson Fidelix <jcksfidelix@gmail.com>
2025-02-05 06:27:55 -0300

    Update aula23

---
+++ pythonTeste/aula023.py
@@ -0,0 +1,7 @@
+# Tratamento de Erros e Exceções
+
+# sintático - exceção
+# "print(x)" nesse caso, x não foi iniciado anteriormente 
+n = int(input('Número: ')) #Digitei 'OITO'
+# "print(n)" nesse caso, n não é um inteiro e apresenta ValueError
+#ZeroDivisionError, no caso de divisões por 0, conforme o próprio nome
