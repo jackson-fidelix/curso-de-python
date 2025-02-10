1538eb830ad951daa61c5304564714bfcfa0513d
Jackson Fidelix <jcksfidelix@gmail.com>
2025-02-10 06:55:12 -0300

    Create ex114.py

--- exercícios/ex113.py
+++ exercícios/ex113.py
@@ -1,4 +1,4 @@
-# Reescrewva a função leiaInt() que fizemos no desafio 104, incluindo
+# Reescreva a função leiaInt() que fizemos no desafio 104, incluindo
 # agora a possibilidade da digitação de um número de tipo inválido.
 # Aproveite e crie também uma função leiaFloat() com a mesma funcionalidade.
 
---
+++ exercícios/ex114.py
@@ -0,0 +1,13 @@
+# Crie um código em Python que teste se o site Pudim está acessível
+# pelo computador usado.
+
+import requests
+import webbrowser
+
+try:
+    online = requests.get('https://www.pudim.com.br/')
+    if online:
+        print('\033[1;32mConsegui acessar o site Pudim com sucesso\033[m')
+        webbrowser.open('https://www.pudim.com.br/')
+except:
+    print('\033[1;31mO site Pudim não está acessível no momento\033[m')
