2193a144444ee50a052f8a050a9f218651bdc994
Jackson Fidelix <116315846+jackson-fidelix@users.noreply.github.com>
2025-02-14 16:28:53 -0300

    Update __init__.py

--- exercícios/ex115/lib/arquivo/__init__.py
+++ exercícios/ex115/lib/arquivo/__init__.py
@@ -12,9 +12,12 @@ def arquivoExiste(nome):
 
 def criarAquivo(nome):
     try:
+        #coloque o caminho onde deseja criar o arquivo
         caminho = r'e:/MeusProjetos/curso-de-python/exercícios/ex115/dados.txt'
-        a = open(caminho, 'wt+')
+        a = open(caminho, 'x')
         a.close()
+    except FileExistsError:
+        print(f'\033[7;32m O arquivo {nome} já existe! \033[m')
     except:
         print('Houve algum ERRO na criação do arquivo!')
     else:
@@ -22,10 +25,34 @@ def criarAquivo(nome):
 
 def lerArquivo(caminho):
     try:
+        #coloque o caminho onde o arquivo está localizado
         caminho = r'e:/MeusProjetos/curso-de-python/exercícios/ex115/dados.txt'
         a = open(caminho, 'rt')
     except:
         print('Erro ao ler arquivo!')
     else:
         cabeçalho('PESSOAS CADASTRADAS')
-        print(a.readlines())
+        for linha in a:
+            dado = linha.split(';')
+            dado[1] = dado[1].replace('\n', '')
+            print(f'{dado[0]:<30}{dado[1]:>3} anos')
+    finally:
+        a.close()
+
+
+def cadastrar(arq, nome='desconhecido', idade=0):
+    try:
+        #coloque o caminho onde o arquivo está localizado
+        caminho = r'e:/MeusProjetos/curso-de-python/exercícios/ex115/dados.txt'
+        a = open(caminho, 'at')
+    except:
+        print(f'Houve um ERRO na abertura do arquivo {arq}!')
+    else:
+        print('Arquivo aberto.')
+        try:
+            a.write(f'{nome};{idade}\n')
+        except:
+            print('Houve um erro na inserção dos dados!')
+        else:
+            print(f'Novo registro de {nome} adicionado.')
+            a.close
