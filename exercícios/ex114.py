6305a41a8c8931e35e76529c22a0ed1575993b39
Jackson Fidelix <jcksfidelix@gmail.com>
2025-02-17 20:05:35 -0300

    Update ex114

--- exercícios/ex114.py
+++ exercícios/ex114.py
@@ -7,7 +7,7 @@ import webbrowser
 try:
     online = requests.get('https://www.pudim.com.br/')
     if online:
-        print('\033[1;32mConsegui acessar o site Pudim com sucesso\033[m')
+        print('\033[1;32mConsegui acessar o site Pudim com sucesso!\033[m')
         webbrowser.open('https://www.pudim.com.br/')
 except:
     print('\033[1;31mO site Pudim não está acessível no momento\033[m')
