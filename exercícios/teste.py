0d273fa6aab0741568b42dbb4273c3283cff663c
Jackson Fidelix <jcksfidelix@gmail.com>
2025-02-17 22:07:06 -0300

    Create teste.py

---
+++ exercícios/teste.py
@@ -0,0 +1,23 @@
+# ex01
+letras =  ('J', 'X', 'M', 'O', 'A', 'K')
+print(letras.index('A'))
+
+# ex02
+num = [2,8,4,7]
+num.pop()
+num.insert(1,3)
+num.append(6)
+print(num)
+
+# ex03
+val = list(range(1, 5))
+print(val)
+
+# ex04 
+num01 = [6, 2, 1, 4, 3]
+num01.sort()
+print(num01)
+
+# ex05
+pontos = (4,8,7,1,9,0,2,7)
+print(sorted(pontos))
